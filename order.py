17b29c0838972e7867899e27559fd1e4d7990890
Vyni <vyni.brasil@gmail.com>
2023-10-07 00:10:39 -0300

    Classe order

--- order.py
+++ order.py
@@ -3,3 +3,8 @@ class order:
         self.total_price = preco_total
         self.status = status
         self.client = cliente   
+
+    def __str__(self):
+        return "order(preco_total= '{}', status = '{}', cliente = '{}')".format(
+            self.preco_total, self.status, self.cliente 
+        )      
